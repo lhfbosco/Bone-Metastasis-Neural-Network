2b311e4b5c2e78f995eabebbc923e128da9f3bac
LotteWong <LotteWong21@gmail.com>
2019-09-06 09:39:52 +0800

    update image process part

--- demo/Network.py
+++ demo/Network.py
@@ -1,34 +1,34 @@
 import torch.nn as nn
 
 
-class FCN(nn.Module):
+class fcn(nn.module):
     def __init__(self):
-        super(FCN, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.MaxPool2d(kernel_size=(4, 4, 4)),
-            nn.Conv2d(25, 25, kernel_size=(3, 3, 3)),
-            nn.Conv2d(25, 25, kernel_size=(3, 3, 3), dilation=(1, 2, 2)),
-            nn.Conv2d(25, 25, kernel_size=(3, 3, 3), dilation=(2, 4, 4)),
-            nn.MaxUnpool2d(25, 25, kernel_size=(4, 4, 4))
+        super(fcn, self).__init__()
+        self.layer1 = nn.sequential(
+            nn.maxpool2d(kernel_size=(4, 4, 4)),
+            nn.conv2d(25, 25, kernel_size=(3, 3, 3)),
+            nn.conv2d(25, 25, kernel_size=(3, 3, 3), dilation=(1, 2, 2)),
+            nn.conv2d(25, 25, kernel_size=(3, 3, 3), dilation=(2, 4, 4)),
+            nn.maxunpool2d(25, 25, kernel_size=(4, 4, 4))
         )
-        self.layer2 = nn.Sequential(
-            nn.MaxPool2d(kernel_size=(4, 4, 4)),
-            nn.Conv2d(25, 25, kernel_size=(3, 3, 3)),
-            nn.Conv2d(25, 25, kernel_size=(3, 3, 3), dilation=(1, 2, 2)),
-            nn.Conv2d(25, 25, kernel_size=(3, 3, 3), dilation=(2, 4, 4)),
-            nn.MaxUnpool2d(25, 25, kernel_size=(2, 2, 2))
+        self.layer2 = nn.sequential(
+            nn.maxpool2d(kernel_size=(4, 4, 4)),
+            nn.conv2d(25, 25, kernel_size=(3, 3, 3)),
+            nn.conv2d(25, 25, kernel_size=(3, 3, 3), dilation=(1, 2, 2)),
+            nn.conv2d(25, 25, kernel_size=(3, 3, 3), dilation=(2, 4, 4)),
+            nn.maxunpool2d(25, 25, kernel_size=(2, 2, 2))
         )
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(3, 25, kernel_size=(3, 3, 3)),
-            nn.Conv2d(25, 25, kernel_size=(3, 3, 3), dilation=(1, 2, 2)),
-            nn.Conv2d(25, 25, kernel_size=(3, 3, 3), dilation=(2, 4, 4))
+        self.layer3 = nn.sequential(
+            nn.conv2d(3, 25, kernel_size=(3, 3, 3)),
+            nn.conv2d(25, 25, kernel_size=(3, 3, 3), dilation=(1, 2, 2)),
+            nn.conv2d(25, 25, kernel_size=(3, 3, 3), dilation=(2, 4, 4))
         )
-        self.layer4 = nn.Sequential(
-           nn.Conv2d(75, 75, kernel_size=(3, 3, 3), dilation=(2, 4, 4)),
-           nn.Conv2d(75, 100, kernel_size=(3, 3, 3), dilation=(2, 4, 4)),
-           nn.Conv2d(100, 100, kernel_size=(3, 3, 3), dilation=(2, 4, 4)),
-           nn.Conv2d(100, 100, kernel_size=(3, 3, 3), dilation=(2, 4, 4)),
-           nn.Conv2d(100, 2, kernel_size=(1, 1, 1))
+        self.layer4 = nn.sequential(
+           nn.conv2d(75, 75, kernel_size=(3, 3, 3), dilation=(2, 4, 4)),
+           nn.conv2d(75, 100, kernel_size=(3, 3, 3), dilation=(2, 4, 4)),
+           nn.conv2d(100, 100, kernel_size=(3, 3, 3), dilation=(2, 4, 4)),
+           nn.conv2d(100, 100, kernel_size=(3, 3, 3), dilation=(2, 4, 4)),
+           nn.conv2d(100, 2, kernel_size=(1, 1, 1))
         )
 
     def forward(self,x):
---
+++ demo/Readme.txt
@@ -0,0 +1,16 @@
+Readme.txt
+
+DL_save_nifti.py
+convert 2D image(.png) to 3D image(.nii)
+The organization of the files and folders should be corresponding to the codes when using the script.
+
+Following steps needed to be done:
+1. subtract 32768 for each pixels
+2. CT Windowing
+The details can be seen in the FAQ.pdf.
+The provided python scripts can be found in the GitHub.
+
+The sample of the processed image is '004458_01_01_005.png'
+
+大家加油鸭，有问题在群里一起讨论
+下周二或者三，我们开个组会康康大家
